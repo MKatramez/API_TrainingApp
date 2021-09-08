6527b0a6969eff1b0cc08339d9a2bb521efd66f5
Mohamad Katramez <mkatramez@evoila.de>
2021-09-08 09:55:10 +0200

    Initial project version

---
+++ demo/src/main/java/com/example/demo/employee/EmployeeConfig.java
@@ -0,0 +1,33 @@
+package com.example.demo.employee;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class EmployeeConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner (EmployeeRepository repository){
+        return args -> {
+            Employee employee1 = new Employee(
+                    "Mohamad",
+                    "mohamad@gmail.com",
+                    LocalDate.of(1998, Month.JANUARY, 28)
+            );
+            Employee employee2 = new Employee(
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(1990, Month.JANUARY, 28)
+            );
+
+            repository.saveAll(
+                    List.of(employee1, employee2)
+            );
+        };
+    }
+}
---
+++ demo/src/main/java/com/example/demo/employee/EmployeeService.java
@@ -0,0 +1,59 @@
+package com.example.demo.employee;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+
+
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public List<Employee> getEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public void addNewEmployee(Employee employee) {
+        Optional<Employee> employeeOptional = employeeRepository.findEmployeeByEmail(employee.getEmail());
+        if(employeeOptional.isPresent()){
+            throw new IllegalStateException("email taken");
+        }
+
+        employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(Long employeeid) {
+        boolean exists = employeeRepository.existsById(employeeid);
+        if (!exists){
+            throw new IllegalStateException("employee with id " + employeeid + " does not exists" );
+        }
+        employeeRepository.deleteById(employeeid);
+    }
+
+    @Transactional
+    public void updateEmployee(Long employeeId, String name, String email) {
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(() -> new IllegalStateException(
+                "employee with id "+ employeeId + " does not exists"
+        ));
+        if (name != null && name.length() > 0 && !Objects.equals(employee.getName(), name)){
+            employee.setName(name);
+        }
+        if (email != null && email.length() > 0 && !Objects.equals(employee.getEmail(), email)){
+            Optional<Employee> employeeOptional = employeeRepository.findEmployeeByEmail(email);
+            if (employeeOptional.isPresent()){
+                throw new IllegalStateException("email taken");
+            }
+            employee.setEmail(email);
+        }
+    }
+}
